7f5a761ca7efffa1a3bc91a433594fb7845b15f4
divyanaC999 <divyachourasiya999@gmail.com>
2022-03-07 01:22:23 +0530

    first commit

---
+++ src/com/bridgelabz/workshop2/DeckOfCardsMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.workshop2;
+
+public class DeckOfCardsMain {
+	public static void main(String[] args) {
+		DeckOfCards deckOfCardsGame = new DeckOfCards();
+		deckOfCardsGame.welcome();
+		deckOfCardsGame.deckOfCards();
+	}
+}
+
